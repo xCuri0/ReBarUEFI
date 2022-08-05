edf436d659d3ad201ffcfc0bfc9e71e1840c8296
Curi0 <zkqri0@gmail.com>
2022-08-05 21:50:27 +0530

    fix linux build

--- buildffs.py
+++ buildffs.py
@@ -8,9 +8,10 @@ import glob
 name = "ReBarDxe"
 version = "1.0"
 GUID = "a8ee1777-a4f5-4345-9da4-13742084d31e"
+shell = sys.platform == "win32"
 
 os.chdir("..")
-subprocess.run(["build", "--platform=ReBarUEFI/ReBar.dsc", "--arch=X64", "--buildtarget=RELEASE"], shell=True, env=os.environ, stderr=sys.stderr, stdout=sys.stdout)
+subprocess.run(["build", "--platform=ReBarUEFI/ReBar.dsc", "--arch=X64", "--buildtarget=RELEASE"], shell=shell, env=os.environ, stderr=sys.stderr, stdout=sys.stdout)
 
 ReBarDXE = glob.glob("./Build/ReBarUEFI/RELEASE_*/X64/ReBarDxe.efi")
 
@@ -18,8 +19,8 @@ if len(ReBarDXE) != 1:
     print("Build failed")
     sys.exit(1)
 
+print(ReBarDXE[0])
 print("Building FFS")
-
 os.chdir(os.path.dirname(ReBarDXE[0]))
 
 try:
@@ -29,9 +30,9 @@ try:
 except FileNotFoundError:
     pass
 
-subprocess.run(["GenSec", "-o", "pe32.sec", "ReBarDxe.efi", "-S", "EFI_SECTION_PE32"], shell=True, env=os.environ, stderr=sys.stderr, stdout=sys.stdout)
-subprocess.run(["GenSec", "-o", "name.sec", "-S", "EFI_SECTION_USER_INTERFACE", "-n", name], shell=True, env=os.environ, stderr=sys.stderr, stdout=sys.stdout)
-subprocess.run(["GenFfs", "-g", GUID, "-o", "ReBarDxe.ffs", "-i", "pe32.sec", "-i" ,"name.sec", "-t", "EFI_FV_FILETYPE_DRIVER"], shell=True, env=os.environ, stderr=sys.stderr, stdout=sys.stdout)
+subprocess.run(["GenSec", "-o", "pe32.sec", "ReBarDxe.efi", "-S", "EFI_SECTION_PE32"], shell=shell, env=os.environ, stderr=sys.stderr, stdout=sys.stdout)
+subprocess.run(["GenSec", "-o", "name.sec", "-S", "EFI_SECTION_USER_INTERFACE", "-n", name], shell=shell, env=os.environ, stderr=sys.stderr, stdout=sys.stdout)
+subprocess.run(["GenFfs", "-g", GUID, "-o", "ReBarDxe.ffs", "-i", "pe32.sec", "-i" ,"name.sec", "-t", "EFI_FV_FILETYPE_DRIVER"], shell=shell, env=os.environ, stderr=sys.stderr, stdout=sys.stdout)
 
 try:
     os.remove("pe32.sec")
